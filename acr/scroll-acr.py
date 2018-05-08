61891e4c6fa5529f505b0b5a6ff9953adc657597
Sajay Antony <sajaya@microsoft.com>
2018-05-08 12:31:53 -0700

    Added acr scroll sample

---
+++ acr/scroll-acr.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+import time
+
+import scrollphat
+
+
+scrollphat.set_brightness(2)
+text = "ACR"
+
+scrollphat.write_string(text, 11)
+
+while True:
+    try:
+        scrollphat.scroll()
+        time.sleep(0.1)
+    except KeyboardInterrupt:
+        scrollphat.clear()
+        sys.exit(-1)
